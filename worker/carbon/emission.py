88557cb8210519218aef840eb8fd386ca43beb7d
Baptiste Sola <sola.baptiste@gmail.com>
2021-08-10 22:26:38 +0200

    differentiate CO2 for planes based on court moyen long courrier

--- worker/carbon/emission.py
+++ worker/carbon/emission.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from loguru import logger
 import os
+from .. import constants
 
 # DEFAULT VALUES
 DEFAULT_CITY = ('', '', '')
@@ -22,20 +23,26 @@ FUEL = "fuel"
 CARBON_DF_PATH = 'worker/carbon/emission.csv'
 
 
-def calculate_co2_emissions(type_transport, nb_km, type_city=None, fuel=None, nb_seats=None):
+def calculate_co2_emissions(type_transport, distance_m, type_city=None, fuel=None, nb_seats=None):
     # read csv
     carbon_df = pd.read_csv(CARBON_DF_PATH, delimiter=',')
     carbon_df = carbon_df[carbon_df[TYPE_OF_TRANSPORT] == type_transport]
 
     if type_city is not None:
         carbon_df = carbon_df[carbon_df[CITY] == type_city[0]]
-        filter_city_size = (carbon_df.loc[:, CITY_SIZE_MIN] >= float(type_city[1])) & (carbon_df.loc[:, CITY_SIZE_MAX] < float(type_city[2]))
+        filter_city_size = (carbon_df.loc[:, CITY_SIZE_MIN] >= float(type_city[1])) &\
+                           (carbon_df.loc[:, CITY_SIZE_MAX] < float(type_city[2]))
         carbon_df = carbon_df[filter_city_size]
 
-    if fuel is not None:
+    # For Planes we differentiate for court, moyen and long courrier
+    if type_transport == constants.TYPE_PLANE:
+        carbon_df = carbon_df[(carbon_df[NB_KM_MIN] < distance_m/1000) & (carbon_df[NB_KM_MAX] > distance_m/1000)]
+
+    if fuel is None:
+        # We take the avg value then
+        carbon_df = carbon_df[(carbon_df[FUEL] == 'avg') | (pd.isna(carbon_df[FUEL]))]
+    else:
         carbon_df = carbon_df[carbon_df[FUEL] == fuel]
 
-    if nb_seats is not None:
-        filter_nb_seats = (carbon_df[NB_SEATS_MIN] <= float(nb_seats)) & (carbon_df[NB_SEATS_MAX] >= float(nb_seats))
-        carbon_df = carbon_df[filter_nb_seats]
-    return carbon_df['value'].mean() * nb_km
+    # The result will be in grams of CO2
+    return carbon_df['value'].mean() * distance_m
