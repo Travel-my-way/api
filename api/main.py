52a3ce74bc0e6a4dd6781ccc13ae9bb36d93b248
Pierre-Yves Gillier <github@pygillier.me>
2021-09-16 22:11:51 +0200

    API: Tools router

--- api/main.py
+++ api/main.py
@@ -1,29 +1,25 @@
 from fastapi import FastAPI
 from .bv import logging as bv_logging
 from .bv.settings import Settings
-from api.bv.routers import journey
+from api.bv.routers import journey, tools
 import logging
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
-config_path=Path(__file__).with_name("logging_config.json")
+config_path = Path(__file__).with_name("logging_config.json")
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    api = FastAPI()
 
-    app.settings = Settings()
-    app.include_router(journey.router)
-    logger = bv_logging.CustomizeLogger.make_logger(config_path)
-    app.logger = logger
+    api.settings = Settings()
+    api.include_router(journey.router)
+    api.include_router(tools.router)
+    log = bv_logging.CustomizeLogger.make_logger(config_path)
+    api.logger = log
 
-    return app
+    return api
 
 
 app = create_app()
-
-
-@app.get("/tools/healthz")
-def healthcheck():
-    return {"status": "ok", "message": "This is the healthcheck endpoint."}
