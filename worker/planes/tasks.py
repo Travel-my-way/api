8c5bcb7be392888264bbba1eb767a14dd084a5de
baptistesola2 <26203850+baptistesola2@users.noreply.github.com>
2022-01-24 15:54:10 +0100

    fic carbon computing for flights with transfer

--- worker/planes/tasks.py
+++ worker/planes/tasks.py
@@ -196,8 +196,9 @@ def plane_journey(plane_trips):
         elif row.nb_step == 2:
             # Trip with transfert
             local_emissions = emission.calculate_co2_emissions(
-                constants.TYPE_PLANE, distance_m
-            )
+                constants.TYPE_PLANE, row.distance_m_first) + \
+                              emission.calculate_co2_emissions(
+                                  constants.TYPE_PLANE, row.distance_m_sec)
             journey_steps = list()
             # first leg
             journey_step = TMW.Journey_step(
